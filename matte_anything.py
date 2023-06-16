3631e973f95298e50c5d4af2029e5b79b36245bf
Pradeep Gaidhani <pradeep.gaidhani@gmail.com>
2023-06-17 00:57:38 +0530

    fix bug for device

--- matte_anything.py
+++ matte_anything.py
@@ -178,7 +178,7 @@ if __name__ == "__main__":
                 h, w, _ = input_x.shape
                 fg_boxes = torch.Tensor(fg_boxes).to(device)
                 fg_boxes = fg_boxes * torch.Tensor([w, h, w, h]).to(device)
-                fg_boxes = box_convert(boxes=fg_boxes, in_fmt="cxcywh", out_fmt="xyxy").numpy()
+                fg_boxes = box_convert(boxes=fg_boxes, in_fmt="cxcywh", out_fmt="xyxy")
 
         else:
             fg_boxes = None
